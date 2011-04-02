3cebee4167b83e1ea99aeacbb3d6adc32639c6f7
antonio carvalho <antoniocarvalho800@gmail.com>
2011-04-02 19:36:50 +0100

    dotfiles

---
+++ awesome/theme.lua
@@ -0,0 +1,23 @@
+theme = {}
+theme.wallpaper_cmd = { "awsetbg /home/apc/images/wall.jpg" }
+
+theme.font      = "profont 8"
+
+theme.fg_normal = "#f8f8f2"
+theme.fg_focus  = "#262729"
+theme.fg_urgent = "#262729"
+theme.fg_title  = "#66d9ef"
+theme.fg_disabled = "#262729"
+theme.bg_normal = "#262729"
+theme.bg_focus  = "#a6e22e"
+theme.bg_urgent = "#f92671"
+theme.bg_disabled = "#5e7175"
+theme.bg_hover = "#5e7175"
+
+theme.border_width     = 0
+theme.border_normal     = "#505050"
+theme.border_focus    = "#292929"
+theme.border_marked    = "#ce5666"
+
+return theme
+
